6b39d43f40821419e98adb234a3ad4851bf75be5
samwick07 <samwick07@gmail.com>
2023-09-11 21:10:14 -1000

    Built default create-react-app with basic GET api endpoint.

---
+++ server/Api.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const api = express();
+const port = 8080;
+
+api.use(cors());
+api.use(express.json());
+
+const knex = require("knex")(
+  require("./knexfile.js")[process.env.NODE_ENV || "development"]
+);
+
+api.get('/', (req, res) => res.status(200).send(`Z-Prefix CRUD API is listening at http://localhost:${port}!`));
+
+api.listen(port, () => console.log(`Z-Prefix CRUD API is listening at http://localhost:${port}!`));
---
+++ server/knexfile.js
@@ -0,0 +1,47 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      filename: 'postgres://postgres:docker@localhost/inventory_db'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
